13626ab4ae0a1254e8eb3ffb7d08e78d112a80ec
Marvin Berrio <marvinccp@gmail.com>
2024-08-09 00:38:32 +0200

    Update database

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
+const sequelize = new Sequelize(process.env.DATABASE_URL,
+  // process.env.DB_NAME,
+  // process.env.DB_USERNAME,
+  // process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
     dialect: "postgres",
   }
 );
