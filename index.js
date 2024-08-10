9ec5d56588a6a940615333263ab27656e4392c42
Marvin Berrio <marvinccp@gmail.com>
2024-08-11 01:46:09 +0200

    Update cors

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const routerApi = require("./routes/index");
-const { connect } = require('./database/dbSync');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser')
-const Port = process.env.PORT || 3000
-const server  = express()
-server.use(cookieParser())
-server.use(express.json())
+const { connect } = require("./database/dbSync");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const Port = process.env.PORT || 3000;
+const server = express();
+server.use(cookieParser());
+server.use(express.json());
 // const corsPort = 3001
 server.use(
   cors({
-    origin: [`http://localhost:${3001}`, `http://localhost:${3002}`, `http://localhost:${3003}`, `https://masterqgame.vercel.app/`],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type,Authorization'
+    origin: [
+      `http://localhost:${3001}`,
+      `http://localhost:${3002}`,
+      `http://localhost:${3003}`,
+      `https://masterqgame.vercel.app/`,
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
-server.use(
-  cors({
-    origin: [`http://localhost:${3001}`, `http://localhost:${3002}`, `http://localhost:${3003}`, `https://masterqgame.vercel.app/`],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: false,
-  })
-);
-server.use(morgan('dev'))
 
+server.use(morgan("dev"));
 
-
-server.get('/', (req, res) =>{
-    res.json({ respuesta: `Esto va en directo por el puerto ${Port}`})
-})
+server.get("/", (req, res) => {
+  res.json({ respuesta: `Esto va en directo por el puerto ${Port}` });
+});
 routerApi(server);
 
-server.listen(Port, ()=>{
-console.log(`Ok listen in ${Port}`)
-})
+server.listen(Port, () => {
+  console.log(`Ok listen in ${Port}`);
+});
 
 connect();
